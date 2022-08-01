dca0e7fe4ab30034b75deb9eb0a66bd2a0162223
Tmeng <tonymengt@gmail.com>
2022-08-01 15:55:23 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var currDayEl = $("#currentDay");
+
+// function creatRow (){
+//     for (var i = 0; i < 9; i++){
+//         var row =""
+//         row += $("<div>").addClass("row")
+//         containerEl.append(row)
+//     }
+// }
